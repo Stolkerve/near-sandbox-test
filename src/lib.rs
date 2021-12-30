51aa28cdb5100da46dd5d6bfac9e3db15554feb7
stolkerve <>
2021-12-29 20:46:11 -0500

    primer commit

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::LookupMap;
+use near_sdk::{env, near_bindgen};
+
+near_sdk::setup_alloc!();
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct StatusMessage {
+    records: LookupMap<String, String>,
+}
+
+impl Default for StatusMessage {
+    fn default() -> Self {
+        Self {
+            records: LookupMap::new(b"r".to_vec()),
+        }
+    }
+}
+
+#[near_bindgen]
+impl StatusMessage {
+    pub fn set_status(&mut self, message: String) {
+        let account_id = env::signer_account_id();
+        self.records.insert(&account_id, &message);
+    }
+
+    pub fn get_status(&self, account_id: String) -> Option<String> {
+        return self.records.get(&account_id);
+    }
+}
+
